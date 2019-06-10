245e6f081387e00e5ffaf46fcd32443cc88f54ab
Andy Buttaro <abuttaro@veracode.com>
2019-06-09 23:21:00 -0400

    up.asyncController to fix tests. postman examples

--- up-demo-spring-boot/src/main/java/com/github/restup/example/SpringBootDemoApplication.java
+++ up-demo-spring-boot/src/main/java/com/github/restup/example/SpringBootDemoApplication.java
@@ -1,24 +1,10 @@
 package com.github.restup.example;
 
-import com.github.restup.ResourceControllerBuilderDecorator;
-import java.io.Serializable;
-import javax.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.github.restup.controller.ResourceControllerBuilderDecorator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.restup.controller.ResourceController;
-import com.github.restup.controller.model.MediaType;
-import com.github.restup.example.university.Course;
-import com.github.restup.example.university.Student;
-import com.github.restup.example.university.University;
-import com.github.restup.registry.ResourceRegistry;
-import com.github.restup.repository.jpa.JpaRepository;
-import com.github.restup.repository.jpa.JpaRepositoryFactory;
-import com.github.restup.spring.mvc.controller.UpSpringMVCConfiguration;
 
 @SpringBootApplication
 @EntityScan
